4648c999c357052f27ff2d2885448267cae452df
PanchooTumare <panchoo99full@gmail.com>
2020-08-17 10:42:41 -0400

    lala

--- pruebita.c
+++ pruebita.c
@@ -3,4 +3,6 @@ int main(){
     printf ("Esto es una prueba para usar git");
     printf ("Este es un tercer cambio");
     printf ("Este es un cuarto cambio");
+    printf ("laksnd");
+    printf ("holi");
 }
